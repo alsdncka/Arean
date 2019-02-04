0c0ccabde5a26b0e96eff7c9b458946b99da6656
minwoo jung <minwoojung@minwooui-Macmini.local>
2019-02-03 20:54:27 -0800

    cc

---
+++ Arean/MainPageController.swift
@@ -0,0 +1,16 @@
+//
+//  MainPageController.swift
+//  Arean
+//
+//  Created by minwoo jung on 03/02/2019.
+//  Copyright © 2019 WW. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainPageController:UIPageViewController{
+    
+    
+    
+}
---
+++ Arean/cameraController.swift
@@ -0,0 +1,16 @@
+//
+//  cameraController.swift
+//  Arean
+//
+//  Created by minwoo jung on 03/02/2019.
+//  Copyright © 2019 WW. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CameraController:UIViewController{
+    
+    
+    
+}
